3bf13264b09da1d56e169f4ba5cd6807dd783e38
Intesar Mohammed <mdshannan@gmail.com>
2013-01-12 23:05:24 -0800

    sort added

--- src/main/java/com/bia/quran/dao/QuranRepositoryImpl.java
+++ src/main/java/com/bia/quran/dao/QuranRepositoryImpl.java
@@ -1,12 +1,16 @@
 package com.bia.quran.dao;
 
 import com.bia.quran.entity.Quran;
+import com.bia.quran.entity.QuranConstants;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
 import org.apache.log4j.Logger;
+import org.apache.lucene.search.Sort;
+import org.apache.lucene.search.SortField;
 import org.hibernate.search.jpa.FullTextEntityManager;
+import org.hibernate.search.jpa.FullTextQuery;
 import org.hibernate.search.query.dsl.QueryBuilder;
 
 /**
@@ -16,12 +20,8 @@ import org.hibernate.search.query.dsl.QueryBuilder;
 public class QuranRepositoryImpl implements QuranRespositorySearch {
 
     protected static final Logger logger = Logger.getLogger(QuranRepositoryImpl.class);
-    
-    @PersistenceUnit(unitName="quran-pu")
+    @PersistenceUnit(unitName = "quran-pu")
     private EntityManagerFactory emf;
-    
-//    @PersistenceContext
-//    private EntityManager entityManager;
 
     @Override
     public List<Quran> search(String term) {
@@ -29,7 +29,7 @@ public class QuranRepositoryImpl implements QuranRespositorySearch {
         logger.info("entityManager is : " + emf);
 
         EntityManager em = emf.createEntityManager();
-        
+
         FullTextEntityManager fullTextEntityManager =
                 org.hibernate.search.jpa.Search.getFullTextEntityManager(em);
 
@@ -39,14 +39,20 @@ public class QuranRepositoryImpl implements QuranRespositorySearch {
 
         org.apache.lucene.search.Query luceneQuery =
                 b.keyword()
-                .onField("ayahText").boostedTo(3)
+                .onField(QuranConstants.AYAH_TEXT).boostedTo(3)
                 .matching(term)
                 .createQuery();
-        javax.persistence.Query fullTextQuery =
+
+        FullTextQuery fullTextQuery =
                 fullTextEntityManager.createFullTextQuery(luceneQuery);
 
-        List<Quran> result = fullTextQuery.getResultList();
+        org.apache.lucene.search.Sort sort = new Sort(
+                new SortField(QuranConstants.SURA_ID, SortField.INT));
         
+        fullTextQuery.setSort(sort);
+
+        List<Quran> result = fullTextQuery.getResultList();
+
         return result;
     }
 }
---
+++ src/main/java/com/bia/quran/entity/QuranConstants.java
@@ -0,0 +1,11 @@
+package com.bia.quran.entity;
+
+/**
+ *
+ * @author Intesar Mohammed <mdshannan@gmail.com>
+ */
+public class QuranConstants {
+    public static final String AYAH_TEXT = "ayahText";
+    public static final String SURA_ID = "suraId";
+    public static final String VERSE_ID = "verseId";
+}
